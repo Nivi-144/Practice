1c804c13ab3334bdb0cc978c1c14e01ccf62e43e
Nivedita Sen <niveditasen14@gmail.com>
2026-06-21 20:40:10 +0530

    Create yup.c

---
+++ yup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int x[10], a[10] [10],n;
+void nextValue{
+  while(1){
+    x[k]=(x[k-1]%(n+1));
+    if(x[k]==0){
+      return;
+    }
+    if(a[x[k-1][x[k]]!=0)
+      {
+        int j;
+        for(j=1;j<=k;j++){
+          if(x[j]==x[k]){
+            break;
+          }
+          if(j==k)
+          {
+              if(k<n||k==n&&a[x[n]][x[1]]!=0)
+              return;
+          }
+        }
+      }
+  }
+}
