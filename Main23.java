21078761280b0244d5f71a3dbf4e4bca5da420d4
Nivedita Sen <niveditasen14@gmail.com>
2026-06-27 23:24:25 +0530

    Add Main23 class with main method and output

---
+++ Main23.java
@@ -0,0 +1,10 @@
+public class Main23 {
+    public static void main(String[] args) {
+        int age = 21;
+        double price = 99.99;
+        String name = "Alex";
+        
+        System.out.println(name + " is " + age);
+        System.out.println("Total: " + (price * 2));
+    }
+}
