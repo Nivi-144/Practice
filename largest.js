abf4d15935bc4d811101e2a721eb04d6ca8b4b03
Nivedita Sen <niveditasen14@gmail.com>
2026-07-11 23:49:38 +0530

    Create largest.js

---
+++ largest.js
@@ -0,0 +1,11 @@
+let a = 25;
+let b = 40;
+let c = 18;
+
+if (a >= b && a >= c) {
+    console.log(a + " is largest");
+} else if (b >= a && b >= c) {
+    console.log(b + " is largest");
+} else {
+    console.log(c + " is largest");
+}
