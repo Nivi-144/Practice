ff349762191fb4c4c2027b3b0e637cb416517328
Nivedita Sen <niveditasen14@gmail.com>
2026-08-07 22:54:04 +0530

    Create SUPERHEROS.sql to list superheroes and years
    Added a SQL script to retrieve superhero data from the OMNIVERSE table.

---
+++ SUPERHEROS.sql
@@ -0,0 +1,23 @@
+SQL> SELECT * FROM OMNIVERSE;
+
+SUPERHEROS                                         MOVIE_YEAR
+-------------------------------------------------- ----------
+IRON MAN                                                 2008
+HULK                                                     2010
+BLACK WIDOW                                              2011
+THOR                                                     2011
+CAPTAIN AMERICA                                          2011
+HAWKEYE                                                  2012
+GROOT                                                    2014
+STAR-LORD                                                2014
+GAMORA                                                   2014
+DRAX                                                     2014
+ROCKET RACOON                                            2014
+ANT-MAN                                                  2015
+BLACK-PANTHER                                            2016
+SPIDERMAN                                                2016
+DR. STRANGE                                              2016
+WASP                                                     2018
+MS. MARVEL                                               2022
+
+17 rows selected.
