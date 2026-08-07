094cf2d84ed14ec2f1d888d16d8f4d010816c934
Nivedita Sen <niveditasen14@gmail.com>
2026-08-07 22:54:50 +0530

    Add SQL queries for OMNIVERSE movie data

---
+++ omniverse.sql
@@ -0,0 +1,19 @@
+SQL> SELECT * FROM OMNIVERSE WHERE MOVIE_YEAR=2014;
+
+SUPERHEROS                                         MOVIE_YEAR
+-------------------------------------------------- ----------
+GROOT                                                    2014
+STAR-LORD                                                2014
+GAMORA                                                   2014
+DRAX                                                     2014
+ROCKET RACOON                                            2014
+
+SQL> SELECT SUPERHEROS FROM OMNIVERSE WHERE MOVIE_YEAR=2016;
+
+SUPERHEROS
+--------------------------------------------------
+BLACK-PANTHER
+SPIDERMAN
+DR. STRANGE
+
+SQL>
