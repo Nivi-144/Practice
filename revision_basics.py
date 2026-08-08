d6694f8bad7fb05afe33f7ede7a7568217374508
Nivedita Sen <niveditasen14@gmail.com>
2026-08-08 11:44:25 +0530

    Create revision_basics.py

---
+++ revision_basics.py
@@ -0,0 +1,9 @@
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+print(name, age)
+a, b = b, a
+n = int(input())
+print("Even" if n % 2 == 0 else "Odd")
+a, b, c = map(int, input().split())
+print(max(a, b, c))
